da312a2debc6fbac51c4a4915b9e686cc8b9fcc4
Rob van Loon <borrob@me.com>
2015-03-06 21:39:38 +0100

    renamed functions, text edits comments

--- pythonled.py
+++ pythonled.py
@@ -8,56 +8,60 @@ class pythonled(object):
             #print 'Lednummer: %d' %(self.lednumber)
         else:
             print 'This lednumber is not supported. Use 0, 1, 2 or 3'
+            raise ValueError('Unsupported led numner.')
 
-    def ledON(self):
+    def on(self):
         '''Switch the LED on
-        by setting the trigger to 'none' and the brightness to 'high'. You need to sed the trigger
-        to 'none' first or else it  would just brighten the LED and not make it static on''' 
+        by setting the trigger to 'none' and the brightness to 'high'. You need to set the trigger
+        to 'none' first or else it would just brighten the LED and not make it static on''' 
        
         os.system('echo none > /sys/class/leds/beaglebone\:green\:usr%i/trigger' %(self.lednumber))
         os.system('echo 1 > /sys/class/leds/beaglebone\:green\:usr%i/brightness' %(self.lednumber))
 
-    def ledOFF(self):
+    def off(self):
        '''Switch the LED off
-       by setting the trigger to 'none' and the brightness to 'low'. You need to sed the trigger
-       to 'none' first or else it  would just dimm the LED''' 
+       by setting the trigger to 'none' and the brightness to 'low'. You need to set the trigger
+       to 'none' first or else it would just dimm the LED''' 
        
        os.system('echo none > /sys/class/leds/beaglebone\:green\:usr%i/trigger' %(self.lednumber))
        os.system('echo 0 > /sys/class/leds/beaglebone\:green\:usr%i/brightness' %(self.lednumber))
 
-    def ledDIMM(self):
+    def dimm(self):
         '''Set the brightness low
            Use ledOFF to switch off the LED'''
        
         os.system('echo 0 > /sys/class/leds/beaglebone\:green\:usr%i/brightness' %(self.lednumber))
 
-    def ledBRIGHT(self):
+    def bright(self):
         '''Set the brightness high
            Use ledON to switch on the LED'''
        
         os.system('echo 1 > /sys/class/leds/beaglebone\:green\:usr%i/brightness' %(self.lednumber))
 
-    def ledHEARTBEAT(self):
+    def heartbeat(self):
         '''Set the brightness high and pulse the LED with a hearbeat
            '''
        
         os.system('echo 1 > /sys/class/leds/beaglebone\:green\:usr%i/brightness' %(self.lednumber))
         os.system('echo heartbeat > /sys/class/leds/beaglebone\:green\:usr%i/trigger' %(self.lednumber))
 
-    def ledTIMER(self,on,off):
+    def times(self,on,off):
         '''Set the brightness high and pulse the LED with ON time and OFF time (ms).
-           '''
+        '''
         os.system('echo 1 > /sys/class/leds/beaglebone\:green\:usr%i/brightness' %(self.lednumber))
         os.system('echo timer > /sys/class/leds/beaglebone\:green\:usr%i/trigger' %(self.lednumber))
         os.system('echo %i > /sys/class/leds/beaglebone\:green\:usr%i/delay_on' %(on,self.lednumber))
         os.system('echo %i > /sys/class/leds/beaglebone\:green\:usr%i/delay_off' %(off,self.lednumber))
 
-    def ledTRANSIENT(self,state,timer):
+    def transient(self,state,timer):
         '''Set a timer
-         Somehow the timerperiod (ms) is double what you would expect
-         you can start in offstate or in onstate and the stat is toggled after the timerperiod.
-         state: the starting state
-           '''
+        Somehow the timerperiod (ms) is double what you would expect
+        you can start in offstate or in onstate (0 or 1) and the stat is
+        toggled after the timerperiod.
+        
+        :param state: the starting state [0 or 1]
+        :param timer: time [ms]
+         '''
         os.system('echo transient > /sys/class/leds/beaglebone\:green\:usr%i/trigger' %(self.lednumber))
         os.system('echo %i > /sys/class/leds/beaglebone\:green\:usr%i/state' %(state,self.lednumber))
         os.system('echo %d > /sys/class/leds/beaglebone\:green\:usr%i/duration' %(timer,self.lednumber))
@@ -65,13 +69,13 @@ class pythonled(object):
 
 def mainfunction():
     user0 = pythonled(0)
-    user0.ledON()
+    user0.on()
     user1 = pythonled(1)
-    user1.ledHEARTBEAT()
+    user1.heartbeat()
     user2 = pythonled(2)
-    user2.ledTIMER(250,100)
+    user2.timer(250,100)
     user3 = pythonled(3)
-    user3.ledTRANSIENT(0,1000)
+    user3.transient(0,1000)
 
 if __name__ == '__main__':
     mainfunction()
