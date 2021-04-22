7a410c4e833d8d4dbd84f5341c241e1135bb0e91
Mahyar Mirrashed <59240843+mahyarmirrashed@users.noreply.github.com>
2021-04-22 02:51:25 -0500

    Updated to Python 3.6 compatible.

--- pythonled.py
+++ pythonled.py
@@ -1,90 +1,97 @@
-import os
+from os import system
 
-class pythonled(object):
-    """ class: pythonled
-    
-    Controls the LEDs on the beaglebone black.
-    """
+class usr_led():
+    '''
+    Controls the USR LEDs on the BeagleBone Black.
+    '''
 
-    def __init__(self,led):
-        """init procedure
-        
-        initialise with the lednumber (0 to 3) on the beaglebone black
-
-        :param led: lednumber
-        """
-        if (led >= 0 and led <=3):
-            self.lednumber = led
-        else:
-            print 'This lednumber is not supported. Use 0, 1, 2 or 3'
-            raise ValueError('Unsupported led numner.')
-
-    def on(self):
-        '''Switch the LED on
-        by setting the trigger to 'none' and the brightness to 'high'. You need to set the trigger
-        to 'none' first or else it would just brighten the LED and not make it static on''' 
-        os.system('echo none > /sys/class/leds/beaglebone\:green\:usr%i/trigger' %(self.lednumber))
-        os.system('echo 1 > /sys/class/leds/beaglebone\:green\:usr%i/brightness' %(self.lednumber))
-
-    def off(self):
-        '''Switch the LED off
-        by setting the trigger to 'none' and the brightness to 'low'. You need to set the trigger
-        to 'none' first or else it would just dimm the LED''' 
-        os.system('echo none > /sys/class/leds/beaglebone\:green\:usr%i/trigger' %(self.lednumber))
-        os.system('echo 0 > /sys/class/leds/beaglebone\:green\:usr%i/brightness' %(self.lednumber))
-
-    def dimm(self):
-        '''Set the brightness low
-        Use ledOFF to switch off the LED'''
-        os.system('echo 0 > /sys/class/leds/beaglebone\:green\:usr%i/brightness' %(self.lednumber))
-
-    def bright(self):
-        '''Set the brightness high
-        Use ledON to switch on the LED'''
-        os.system('echo 1 > /sys/class/leds/beaglebone\:green\:usr%i/brightness' %(self.lednumber))
-
-    def heartbeat(self):
-        '''Set the brightness high and pulse the LED with a heartbeat
+    def __init__(self, num: int) -> None:
         '''
-        os.system('echo 1 > /sys/class/leds/beaglebone\:green\:usr%i/brightness' %(self.lednumber))
-        os.system('echo heartbeat > /sys/class/leds/beaglebone\:green\:usr%i/trigger' %(self.lednumber))
+        Constructor method to initialize the class with a valid number.
 
-    def times(self,on,off):
-        '''Set the brightness high and pulse the LED with ON time and OFF time (ms).
-
-        :param on: on time (ms)
-        :param off: of time (ms)
+        Arguments:
+            num: LED number (0 to 3) on the BeagleBone Black to control.
         '''
-        os.system('echo 1 > /sys/class/leds/beaglebone\:green\:usr%i/brightness' %(self.lednumber))
-        os.system('echo timer > /sys/class/leds/beaglebone\:green\:usr%i/trigger' %(self.lednumber))
-        os.system('echo %i > /sys/class/leds/beaglebone\:green\:usr%i/delay_on' %(on,self.lednumber))
-        os.system('echo %i > /sys/class/leds/beaglebone\:green\:usr%i/delay_off' %(off,self.lednumber))
+        if not isinstance(num, int):
+            raise ValueError(f'LED number "{num}" is not an integer.')
+        elif num not in range(4):
+            raise ValueError(f'LED number "{num}" is not supported. Use either 0, 1, 2, or 3.')
 
-    def transient(self,state,timer):
-        '''Set a timer
-        Somehow the timerperiod (ms) is double what you would expect
-        you can start in offstate or in onstate (0 or 1) and the stat is
-        toggled after the timerperiod.
-        
-        :param state: the starting state [0 or 1]
-        :param timer: time [ms]
-         '''
-        os.system('echo transient > /sys/class/leds/beaglebone\:green\:usr%i/trigger' %(self.lednumber))
-        os.system('echo %i > /sys/class/leds/beaglebone\:green\:usr%i/state' %(state,self.lednumber))
-        os.system('echo %d > /sys/class/leds/beaglebone\:green\:usr%i/duration' %(timer,self.lednumber))
-        os.system('echo 1 > /sys/class/leds/beaglebone\:green\:usr%i/activate' %(self.lednumber))
+        self.num = num
 
-def mainfunction():
-    """A mainfunction as an example
-    """
-    user0 = pythonled(0)
-    user0.on()
-    user1 = pythonled(1)
-    user1.heartbeat()
-    user2 = pythonled(2)
-    user2.timer(250,100)
-    user3 = pythonled(3)
-    user3.transient(0,1000)
+    def on(self) -> None:
+        '''
+        Switch the LED on by setting the trigger to 'none' and the brightness to 'high'.
+        '''
 
-if __name__ == '__main__':
-    mainfunction()
+        system(f'echo none > sys/class/leds/beaglebone\:green\:usr{self.num}/trigger')
+        system(f'echo 1 > /sys/class/leds/beaglebone\:green\:usr{self.num}/brightness')
+
+    def off(self) -> None:
+        '''
+        Switch the LED off by setting the trigger to 'none' and the brightness to 'low'.
+        '''
+
+        system(f'echo none > /sys/class/leds/beaglebone\:green\:usr{self.num}/trigger')
+        system(f'echo 0 > /sys/class/leds/beaglebone\:green\:usr{self.num}/brightness')
+
+    def dimm(self) -> None:
+        '''
+        Set the brightness of the LED to 'low'.
+        '''
+
+        system(f'echo 0 > /sys/class/leds/beaglebone\:green\:usr{self.num}/brightness')
+
+    def bright(self) -> None:
+        '''
+        Set the brightness of the LED to 'high'.
+        '''
+
+        system(f'echo 1 > /sys/class/leds/beaglebone\:green\:usr{self.num}/brightness')
+
+    def heartbeat(self) -> None:
+        '''
+        Set the bright of the LED to 'high' and pulse the LED with a heartbeat pattern.
+        '''
+
+        system(f'echo 1 > /sys/class/leds/beaglebone\:green\:usr{self.num}/brightness')
+        system(f'echo heartbeat > /sys/class/leds/beaglebone\:green\:usr{self.num}/trigger')
+
+    def times(self, delay_on: int, delay_off: int) -> None:
+        '''
+        Set the birghtness of the LED to 'high' and pulse the LED with 'delay_on' time
+        and 'delay_off' time in milliseconds (ms).
+
+        Arguments:
+            delay_on: Number of milliseconds to keep LED on.
+            delay_off: Number of milliseconds to keep LED off.
+        '''
+
+        if not isinstance(delay_on, int) or delay_on <= 0:
+            raise ValueError(f'Delay value "{delay_on}" is invalid.')
+        if not isinstance(delay_off, int) or delay_off <= 0:
+            raise ValueError(f'Delay value "{delay_off}" is invalid.')
+
+        system(f'echo 1 > /sys/class/leds/beaglebone\:green\:usr{self.num}/brightness')
+        system(f'echo timer > /sys/class/leds/beaglebone\:green\:usr{self.num}/trigger')
+        system(f'echo {delay_on} > /sys/class/leds/beaglebone\:green\:usr{self.num}/delay_on')
+        system(f'echo {delay_off} > /sys/class/leds/beaglebone\:green\:usr{self.num}/delay_off')
+
+    def transient(self, state: bool, period: int) -> None:
+        '''
+        Set a timer period in milliseconds (ms) and set the starting state (0 or 1).
+
+        Arguments:
+            state: Initial state for transient LED.
+            period: Time for one period of the transient.
+        '''
+
+        if not isinstance(state, bool):
+            raise ValueError('State variable must be a boolean.')
+        if not isinstance(period, int) or period <= 0:
+            raise ValueError(f'Period value "{period}" is invalid.')
+
+        system(f'echo transient > /sys/class/leds/beaglebone\:green\:usr{self.num}/trigger')
+        system(f'echo {state} > /sys/class/leds/beaglebone\:green\:usr{self.num}/state')
+        system(f'echo {period} > /sys/class/leds/beaglebone\:green\:usr{self.num}/duration')
+        system(f'echo 1 > /sys/class/leds/beaglebone\:green\:usr{self.num}/activate')
